0671d18f87fc19222a1c29e7ceb9ae57e677a5dc
Drew Norman <drew.norman@camunda.com>
2020-04-18 18:26:42 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 module.exports = {
   title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
+  author: 'Drew Norman', // Required
   description: 'Full-stack Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://drewnorman.com',
+    github: 'https://github.com/jedidrew42/',
+    twitter: 'https://twitter.com/drewbacca46',
+    linkedin: 'https://www.linkedin.com/in/drewnorman/',
   },
   pathPrefix: '/devblog',
   siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
